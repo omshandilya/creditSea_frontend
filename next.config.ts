a0c692ca55a3e013c1042bb68e825f928c8e26ee
your_correct_github_username <your_correct_email@exa>
2025-04-06 23:02:22 +0530

    Initial commit of creditsea fronten

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
